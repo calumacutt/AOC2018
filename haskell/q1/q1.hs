91b4fa40f253e96f0def571501e724b6d9733b5a
calumacutt <calumacutt@gmail.com>
2021-07-04 15:49:55 +1000

    haskell q1 part 2

--- haskell/q1/q1.hs
+++ haskell/q1/q1.hs
@@ -2,7 +2,23 @@ readLine :: String -> Integer
 readLine ('-' : xs) = -1 * read xs
 readLine ('+' : xs) = read xs
 
+headInTail :: [Integer] -> Bool
+headInTail [] = False
+headInTail (x : xs) = x `elem` xs
+
+appendIncrement :: [Integer] -> Integer -> [Integer]
+appendIncrement [] y = [y]
+appendIncrement (x : xs) y = (x + y) : x : xs
+
+findFirstDup = dropWhile (not . headInTail)
+
+genIncResults = scanl appendIncrement []
+
+readToCycledList = cycle . map readLine
+
 main :: IO ()
 main = do
     listOfStrings <- lines <$> readFile "input.txt"
-    print (sum (map readLine listOfStrings))
+    (print . sum . (map readLine)) listOfStrings
+    (print . head . head . findFirstDup . genIncResults . readToCycledList) listOfStrings
+    -- print (head (head (dropWhile (not . headInTail) (scanl appendIncrement [] (cycle (map readLine listOfStrings))))))
